bbb16cb62d3a51a0c7bf9e89e44c512d3070b7a3
vijethx <38877972+vijethx@users.noreply.github.com>
2022-06-20 14:13:34 +0530

    store wave data on smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint totalWaves;
+
   constructor(){
     console.log("Yo yo, I'm a contract and I'm smart");
+  }
+
+  function wave() public{
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() public view returns (uint){
+    console.log("We have %d total waves", totalWaves);
+    return totalWaves;
   }
 }
